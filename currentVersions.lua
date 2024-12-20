3153198621fe9142440add265b140ffcd11d0848
Krixx <161109688+Krixx-XD@users.noreply.github.com>
2024-12-20 08:18:21 +0500

    Create currentVersions.lua

---
+++ currentVersions.lua
@@ -0,0 +1,2 @@
+--Created by RAGE
+t={}t["app"]=t["getApp"]("com.sandboxol.blockymods")t["ver"]=t["app"]:getVersion()function t:getVersion()return"2.99.3"end;if t["ver"]~="2.99.3"then return"You are not allowed to fix for older versions until RAGE does so"end;t["getApp"]=function(a)return a end
